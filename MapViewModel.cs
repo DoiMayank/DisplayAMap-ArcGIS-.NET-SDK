a42bcf5640639f0efaf1438490ee6381dd6ded0e
DoiMayank <doi.mayank@gmail.com>
2024-05-21 11:39:39 +0530

    Add project files.

---
+++ MapViewModel.cs
@@ -0,0 +1,63 @@
+﻿//   Copyright 2021 Esri
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Esri.ArcGISRuntime.Mapping;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DisplayAMap
+{
+
+    internal class MapViewModel : INotifyPropertyChanged
+    {
+
+        public MapViewModel()
+        {
+            SetupMap();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private Map? _map;
+        public Map? Map
+        {
+            get { return _map; }
+            set
+            {
+                _map = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void SetupMap()
+        {
+
+            // Create a new map with a 'topographic vector' basemap.
+            Map = new Map(BasemapStyle.ArcGISTopographic);
+
+        }
+
+    }
+
+}
+
